82625fa5aa6468c9e6e3f167261a2fa47c6eaedc
hyyga <aureliacalista537@gmail.com>
2025-12-04 14:27:03 +0700

    login bener

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    {{-- CSS CDN Example --}}
+    {{-- CSS CDN --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 
     <title>@yield('title', 'WTS')</title>
 </head>
 <body>
 
-    {{-- Content from child views --}}
     @yield('content')
 
-    {{-- JS CDN Example --}}
+    {{-- JS CDN --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
